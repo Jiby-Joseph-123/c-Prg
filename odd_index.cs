5f74ce654a5eab8ad169fd8da61763623083c44a
Jiby Joseph <104261418+Jiby-Joseph-123@users.noreply.github.com>
2023-01-16 17:44:44 +0530

    Add files via upload

---
+++ odd_index.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+decimal[] array = new decimal[10];
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Enter the fare in decimal:");
+   decimal fare = Convert.ToDecimal(Console.ReadLine());
+    for (int j = 0; j < 5; j++)
+    {
+
+
+        if (j % 2 != 0)
+        {
+            array[i] = fare;
+            break;
+        }
+        else
+        {
+            array[i] = 0;
+        }
+    }
+}
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
