94c12bf32a11d8feafdd9f956700b83a8f0a5529
Jiby Joseph <104261418+Jiby-Joseph-123@users.noreply.github.com>
2023-01-19 10:30:11 +0530

    Add files via upload

---
+++ arrayMerge.cs
@@ -0,0 +1,36 @@
+﻿string[] name = new string[5];
+string[] destinstion= new string[5];
+
+for(int i=0; i<5;i++)
+{
+    Console.WriteLine("Enter the name of passenger:");
+    name[i] = Console.ReadLine();
+}
+
+foreach (var item in name)
+{
+    Console.WriteLine(item);
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Enter the destination of passenger:");
+    destinstion[i] = Console.ReadLine();
+}
+
+foreach (var item in destinstion)
+{
+    Console.WriteLine(item);
+}
+
+var myList = new List<string>();
+myList.AddRange(name);
+myList.AddRange(destinstion);
+string[] array = myList.ToArray();
+
+Console.WriteLine("New array: ");
+
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
