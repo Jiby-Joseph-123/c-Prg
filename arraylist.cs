3df20213439ff6a47d48e2aae363e11ca3105f82
Jiby Joseph <104261418+Jiby-Joseph-123@users.noreply.github.com>
2023-01-22 14:56:26 +0530

    Add files via upload

---
+++ arraylist.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+ArrayList flightNum = new ArrayList();
+
+
+while (true)
+{
+    Console.WriteLine("1.Add flight number");
+    Console.WriteLine("2.View");
+    Console.WriteLine("3.quit");
+
+
+    Console.WriteLine("Enter the choice:");
+    int input = Convert.ToInt16(Console.ReadLine());
+   
+    switch (input)
+    {
+        case 1:
+            Console.WriteLine("Enter the flight number:");
+            int num = Convert.ToInt16(Console.ReadLine());
+            flightNum.Add (num);
+           
+            break;
+        case 2:
+            foreach (var item in flightNum)
+            {
+                Console.WriteLine(item);
+            }
+            break;
+        case 3:
+            
+            break;
+        default:
+            Console.WriteLine("Invalid");
+            break;
+    }
+}
